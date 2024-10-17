afd4d5b2be6a32678597c6e7fc6b212b3b9ab922
Sadcato <2921735369@qq.com>
2024-10-17 22:50:57 +0800

    Site updated: 2024-10-17 22:50:56

---
+++ js/title.js
@@ -0,0 +1,17 @@
+//��̬����
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //�뿪��ǰҳ��ʱ��ǩ��ʾ����
+    document.title = '?������ȥ��~';
+    clearTimeout(titleTime);
+  } else {
+    //���ص�ǰҳ��ʱ��ǩ��ʾ����
+    document.title = '?ץ��������';
+    //���������������
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
